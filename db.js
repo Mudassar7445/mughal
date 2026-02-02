24ca88f79327d7d29bc45d1abe6aee14463a9e1c
Mudassar Imran <mudassarimranhafizmudassr@gmail.com>
2026-02-02 22:03:10 +0500

    Fixed db connection with hardcoded credentials

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const { createClient } = require("@libsql/client");
-require("dotenv").config();
 
+// Hum Vercel ki settings use nahi kar rahe, direct yahan likh rahe hain
 const db = createClient({
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  // Maine aapka URL 'https' kar diya hai taake connection foran jurr jaye
+  url: "https://mughal-db-mudassar7445.aws-ap-south-1.turso.io",
+  
+  // Aapka diya hua Token
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzAwNTEyMzEsImlkIjoiOGJlZDE1YWItOWUyNy00MjRiLThjYzMtNmFjN2NiZTIzY2UxIiwicmlkIjoiMjY1YjNiMTgtZDA1YS00OTUxLWI2YzktMDMzYzNlZjFhNjJiIn0.zVBC046n-Ngq-_DGweCU8iHp_gUJDTZUMqJPQzRcgdLhYrq3ID6ltbj8n0HvwvsgnoLbLOWUK7vujFlc7vjvDA"
 });
 
 module.exports = db;
