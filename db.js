b0bab3a5f8e2eec796861ed7cd393d6054d4163e
Mudassar Imran <mudassarimranhafizmudassr@gmail.com>
2026-02-02 22:21:34 +0500

    FORCE UPDATE to HTTPS 100 percent

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { createClient } = require("@libsql/client");
 
 const db = createClient({
-  // Ye HTTPS wala link use karega (Fast & No Migration Error)
+  // Ghaur karein: Yahan 'https' hai. Agar ye 'libsql' hota to wo migration wala error aata.
   url: "https://mughal-db-mudassar7445.aws-ap-south-1.turso.io",
   
-  // Aapka Token (Maine check kar liya hai, ye sahi format mein hai)
+  // Aapka Token (Bilkul sahi hai)
   authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzAwNTEyMzEsImlkIjoiOGJlZDE1YWItOWUyNy00MjRiLThjYzMtNmFjN2NiZTIzY2UxIiwicmlkIjoiMjY1YjNiMTgtZDA1YS00OTUxLWI2YzktMDMzYzNlZjFhNjJiIn0.zVBC046n-Ngq-_DGweCU8iHp_gUJDTZUMqJPQzRcgdLhYrq3ID6ltbj8n0HvwvsgnoLbLOWUK7vujFlc7vjvDA"
 });
 
