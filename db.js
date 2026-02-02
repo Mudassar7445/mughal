8d33e822586c2d105a9ac721811d489c89074018
Mudassar Imran <mudassarimranhafizmudassr@gmail.com>
2026-02-02 22:12:56 +0500

    Fix db connection to use HTTPS

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 const { createClient } = require("@libsql/client");
 
+// Humne aapka diya hua Token aur HTTPS link yahan fix kar diya hai
 const db = createClient({
-  // Wapis libsql use kar rahe hain kyunke https masla kar raha hai
-  url: "libsql://mughal-db-mudassar7445.aws-ap-south-1.turso.io",
-  
-  // Token aapka wala hi hai
+  url: "https://mughal-db-mudassar7445.aws-ap-south-1.turso.io",
   authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzAwNTEyMzEsImlkIjoiOGJlZDE1YWItOWUyNy00MjRiLThjYzMtNmFjN2NiZTIzY2UxIiwicmlkIjoiMjY1YjNiMTgtZDA1YS00OTUxLWI2YzktMDMzYzNlZjFhNjJiIn0.zVBC046n-Ngq-_DGweCU8iHp_gUJDTZUMqJPQzRcgdLhYrq3ID6ltbj8n0HvwvsgnoLbLOWUK7vujFlc7vjvDA"
 });
 
